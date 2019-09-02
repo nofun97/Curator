7eafb87e65785cb8adabb73347ec3865d2efb45c
Patrick Setiawan <seti4w4np4trick@gmail.com>
2019-09-02 23:31:04 +1000

    fixed HomeScreen View

--- App/Components/HomeScreen.js
+++ App/Components/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, StyleSheet, View, Button, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
+import HomeComponent from "./HomeComponent";
 
 // styled components
 const ScreenBox = styled.View`
@@ -17,47 +18,35 @@ const LoginBox = styled.View`
 	flex: 0.65;
 	background-color: #333333;
 	borderColor = black;
-	align-items: center;
+	flex-direction: row
 `;
 
-const LoginStyle = styled.Text`
-    top: 50;
-    borderColor: red;
-    borderWidth: 3;
-`;
-
-const UsernameStyle = styled.Text`
-    top: 100;
-    borderColor: red;
-    borderWidth: 3;
-    right: 100
-`;
-
-const HomeScreen = ({navigation}) => {
+const HomeScreen = (navigation) => {
     return (
         <ScreenBox>
             <ImageBox>
                 <Text> Image goes here </Text>
             </ImageBox>
             <LoginBox>
-                <LoginStyle> Login </LoginStyle>
-                <UsernameStyle> Username </UsernameStyle>
-                <UsernameStyle> Password </UsernameStyle>
+                <HomeComponent top = '0'
+                               left = '0'
+                               right = '0'
+                               text = {'Register'}/>
+                <HomeComponent top = '0'
+                               left = '0'
+                               right = '0'
+                               text = {'Login'}/>
+                <HomeComponent top = '0'
+                               left = '0'
+                               right = '0'
+                               text = {'Username'}/>
+                <HomeComponent top = '0'
+                               left = '0'
+                               right = '0'
+                               text = {'Password'}/>
             </LoginBox>
         </ScreenBox>
     );
 };
-
-const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30
-    }
-});
-
-/*<Button
-                onPress={() => navigation.navigate('Component')}
-                title = "Go to Components Screen"
-            />
-            */
 
 export default HomeScreen
