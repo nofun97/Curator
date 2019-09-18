43b0489c76c90618ee64d3eaf7fa8426bca74cfa
Novan <novan.allanadi1211@gmail.com>
2019-09-18 22:48:50 +1000

    added upload profile controller

--- App/controllers/authentications.js
+++ App/controllers/authentications.js
@@ -5,5 +5,12 @@ export const login = (email, password) => {
 };
 
 export const signUp = (email, password) => {
-  return firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password);
+  return firebase.auth().createUserWithEmailAndPassword(email, password);
+};
+
+export const uploadProfile = user => {
+  return firebase
+    .firestore()
+    .collection('users')
+    .add(user);
 };
