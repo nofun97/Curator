3fdec87fcb7923d3a0234faee6552f7a44191148
Novan Allanadi <novan.allanadi1211@gmail.com>
2019-09-25 16:41:04 +1000

    example app for image picker

--- App/Components/App.js
+++ App/Components/App.js
@@ -1,64 +1,77 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
-import React, {useState, useEffect} from 'react';
-import {Text, View, Image} from 'react-native';
-
-import {viewItem} from '../controllers/items';
+/*This is an example of Image Picker in React Native*/
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
-
-const App = () => {
-  const [user, setUser] = useState('hello there');
-  const [image, setImage] = useState('');
-  const options = {
-    title: 'Select Avatar',
-    customButtons: [{name: 'test', title: 'test'}],
-    storageOptions: {
-      skipBackup: true,
-      path: 'images',
-    },
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filePath: {},
+    };
+  }
+  chooseFile = () => {
+    var options = {
+      title: 'Select Image',
+      customButtons: [
+        { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+ 
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        let source = response;
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+        this.setState({
+          filePath: source,
+        });
+      }
+    });
   };
-
-  /**
-   * The first arg is the options object for customization (it can also be null or omitted for default options),
-   * The second arg is the callback which sends object: response (more info in the API Reference)
-   */
-  ImagePicker.showImagePicker(options, response => {
-    console.log('Response = ', response);
-
-    if (response.didCancel) {
-      console.log('User cancelled image picker');
-    } else if (response.error) {
-      console.log('ImagePicker Error: ', response.error);
-    } else if (response.customButton) {
-      console.log('User tapped custom button: ', response.customButton);
-    } else {
-      const source = {uri: response.uri};
-
-      // You can also display the image using data:
-      // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-      setImage(source);
-    }
-  });
-
-  // useEffect(() => {
-  //   viewItem('jmRD0gUvkB7XAOiY7wr1').then(data =>
-  //     setUser(Object.keys(data.data().owners)[0]),
-  //   );
-  // }, []);
-
-  return (
-    <View>
-      <Text>{image}</Text>
-      <Image source={{uri: image}} />
-    </View>
-  );
-};
-
-export default App;
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.container}>
+          {/*<Image 
+          source={{ uri: this.state.filePath.path}} 
+          style={{width: 100, height: 100}} />*/}
+          <Image
+            source={{
+              uri: 'data:image/jpeg;base64,' + this.state.filePath.data,
+            }}
+            style={{ width: 100, height: 100 }}
+          />
+          <Image
+            source={{ uri: this.state.filePath.uri }}
+            style={{ width: 250, height: 250 }}
+          />
+          <Text style={{ alignItems: 'center' }}>
+            {this.state.filePath.uri}
+          </Text>
+          <Button title="Choose File" onPress={this.chooseFile.bind(this)} />
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
