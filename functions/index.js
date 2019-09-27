b0394bcae0cf7169e3fe5af8b5bcad99af344cae
Novan <novan.allanadi1211@gmail.com>
2019-09-27 23:41:45 +1000

    Functions delete original file now

--- functions/index.js
+++ functions/index.js
@@ -5,30 +5,17 @@ const spawn = require("child-process-promise").spawn;
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
+
 exports.compressImages = functions.storage.object().onFinalize(async object => {
-  // [END generateThumbnailTrigger]
-  // [START eventAttributes]
   const fileBucket = object.bucket; // The Storage bucket that contains the file.
   const filePath = object.name; // File path in the bucket.
   const contentType = object.contentType; // File content type.
-  const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
-  // [END eventAttributes]
-
-  // [START stopConditions]
-  // Exit if this is triggered on a file that is not an image.
-  // if (!contentType.startsWith("image/")) {
-  //   return console.log("This is not an image.");
-  // }
 
   // Get the file name.
   const fileName = path.basename(filePath);
   if (fileName.startsWith("compressed_")) {
     return console.log("Already compressed.");
   }
-  // [END stopConditions]
-
-  // [START thumbnailGeneration]
-  // Download file from bucket.
   const bucket = admin.storage().bucket(fileBucket);
   const tempFilePath = path.join(os.tmpdir(), fileName);
   const metadata = {
@@ -36,7 +23,8 @@ exports.compressImages = functions.storage.object().onFinalize(async object => {
   };
   await bucket.file(filePath).download({ destination: tempFilePath });
   console.log("Image downloaded locally to", tempFilePath);
-  // Generate a thumbnail using ImageMagick.
+
+  // compression command line
   await spawn("convert", [
     "-strip",
     "-interlace",
@@ -48,8 +36,8 @@ exports.compressImages = functions.storage.object().onFinalize(async object => {
     tempFilePath,
     tempFilePath,
   ]);
+
   console.log("Image is compressed");
-  // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
   const compressedFileName = `compressed_${fileName}`;
   const compressedFilePath = path.join(
     path.dirname(filePath),
@@ -61,8 +49,8 @@ exports.compressImages = functions.storage.object().onFinalize(async object => {
     metadata: metadata,
   });
   console.log("Image is uploaded back to storage");
-  // Once the thumbnail has been uploaded delete the local file to free up disk space.
+  await bucket.file(filePath).delete();
+  console.log("Original image is deleted");
+  
   return fs.unlinkSync(tempFilePath);
-  // [END thumbnailGeneration]
 });
-// [END generateThumbnail]
