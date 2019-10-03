a9004590facce9dd900153511ccf116f66634a89
Patrick Setiawan <seti4w4np4trick@gmail.com>
2019-10-03 21:39:36 +1000

    fixed weird stuff happening

--- App/Components/Pages/RegisterPage.js
+++ App/Components/Pages/RegisterPage.js
@@ -6,8 +6,8 @@ export default class RegisterPage extends Component{
   render(){
     return(
 		  <View style={styles.ViewStyle}>
-			  <Text style={styles.Title}>
-			 	 Enter your information below
+			  <Text style={styles.TextStyle}>
+			 	 Enter your information below:
 		  	  </Text>
 			  <RegisterForm/>
 		  </View>
@@ -16,21 +16,11 @@ export default class RegisterPage extends Component{
 }
 
 const styles = StyleSheet.create({
+	TextStyle: {
+		color : "#c8dede"
+	},
 	ViewStyle: {
-<<<<<<< HEAD
 		flex : 1,
 		backgroundColor : '#264242'
 	}
-=======
-		flex : 1 ,
-		backgroundColor : '#233838',
-	},
-	Title: {
-		fontSize: 24,
-		color: 'white',
-		marginTop: 10,
-		marginLeft: 10,
-		fontFamily: 'proxima_nova_semibold',
-	},
->>>>>>> f7b2a35b913fed7c2c8e17eae0a26e3f1035e2cf
 });
