8753f69c3d6514a5e5c63e20ad66fe714d03e958
Saint_Tan <tanhwaisaint@hotmail.com>
2019-10-10 07:34:37 +1100

    changed AppStyles names for easier implementation later

--- App/Components/AppStyles.js
+++ App/Components/AppStyles.js
@@ -2,28 +2,37 @@ import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  inputTextStyles: {
+  baseTextStyle:{
+    color: '#ffffff',
+    fontFamily: 'Montserrat',
+  },
+  baseButtonBackStyle:{
+    backgroundColor: '#5f9999',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputTextStylesLogin: {
     marginBottom: 10,
     color: '#d4d4d4',
     alignItems: 'center',
     width: 260,
   },
-  textStyle: {
-    color: '#ffffff',
-    fontFamily: 'Montserrat',
-  },
-  registerViewStyle: {
+  // textStyle: {
+  //   color: '#ffffff',
+  //   fontFamily: 'Montserrat',
+  // },
+  registerViewStyleLogin: {
     justifyContent: 'center',
     alignItems: 'center',
   },
-  viewStyle: {
+  viewStyleLogin: {
     marginTop: 15,
   },
-  buttonTextStyle: {
-    color: '#ffffff',
-    fontFamily: 'Montserrat',
-  },
-  loginButtonStyle: {
+  // buttonTextStyle: {
+  //   color: '#ffffff',
+  //   fontFamily: 'Montserrat',
+  // },
+*  loginButtonStyleLogin: {
     marginTop: 30,
     marginBottom: 35,
     height: 50,
@@ -32,7 +41,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  registerButtonStyle: {
+*  registerButtonStyleLogin: {
     marginTop: 10,
     height: 50,
     width: 175,
@@ -55,7 +64,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  ButtonStyle: {
+*  ButtonStyle: {
     width: 225,
     marginTop: 30,
     height: 50,
@@ -75,4 +84,40 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     marginTop: 10,
   },
+  TextStyle: {
+    color: '#c8dede',
+    marginTop: 20,
+  },
+  ViewStyle: {
+    flex: 1,
+    backgroundColor: '#264242',
+  },
+  ScreensBox: {
+    flex: 1,
+  },
+  LogoContainer: {
+    flex: 0.35,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  LogoStyle: {
+    height: 100,
+  },
+  TitleStyle: {
+    color: '#1d2e2e',
+    marginTop: 10,
+    textAlign: 'center',
+    fontSize: 40,
+    opacity: 0.9,
+    paddingBottom: 15,
+  },
+  FormContainer: {
+    flex: 0.65,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#264242',
+  },
+
+
+
 });
