443ee25e94ec369d58c1b059a24babde13bd1c80
Novan <novan.allanadi1211@gmail.com>
2019-10-10 13:03:49 +1100

    fixed syntax error

--- App/Pages/ItemRegistrationPage.js
+++ App/Pages/ItemRegistrationPage.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import ItemRegistrationForm from '../Components/ItemRegistrationForm';
 import ItemRegistrationTop from '../Components/ItemRegistrationTop';
 
 export default class ItemRegistrationPage extends Component{
   render(){
-    return(
-      <View style= styles.viewContainer>
-        <ItemRegistrationTop style = styles.itemRegistrationTopStyle/>
-        <ItemRegistrationForm style = styles.itemRegistrationFormStyle/>
+    return (
+      <View style= {styles.viewContainer}>
+        <ItemRegistrationTop style = {styles.itemRegistrationTopStyle}/>
+        <ItemRegistrationForm style = {styles.itemRegistrationFormStyle}/>
       </View>
     );
   }
