c56df37747991b44f55b08816c9b83f0b2767d58
Novan Allanadi <novan.allanadi1211@gmail.com>
2019-10-20 22:40:56 +1100

    added item registration

--- App/Components/ItemRegistrationForm.js
+++ App/Components/ItemRegistrationForm.js
@@ -5,30 +5,125 @@ import {
   StyleSheet,
   TouchableOpacity,
   SafeAreaView,
-  ScrollView
+  ScrollView,
+  ActivityIndicator,
+  Button,
+  Image,
 } from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import {registerItem} from '../controllers/items';
+import {connect} from 'react-redux';
+import moment from 'moment';
+import ImagePicker from 'react-native-image-picker';
 
-export default class ItemRegistrationForm extends Component{
+class ItemRegistrationForm extends Component{
   constructor(props){
     super(props);
     this.state = {
       name: '',
-      owner: '',
-      dateAdded: '',
-      origin: '',
+      owners: [], //TODO: need to be a list of {firstName: string, lastName: string, id: string}
+      dateOwned: moment(),
+      // origin: '',  Not used?
       description: '',
+      photos: [], // a list of items
+      categories: ['placeholder'], //TODO: needs to be a list
+      warning: '',
+      isUploadingImage: false,
+      isUploading: false,
+      // TODO: make this look better
+      finishedMessage: <Text>Uploading is complete! You can continue doing anything else</Text>,
+      showFinishedMessage: false,
     };
     this.onPressHandler = this.onPressHandler.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.onAddImage = this.onAddImage.bind(this);
+    this.renderImages = this.renderImages.bind(this);
+    this.goBack = this.goBack.bind(this);
   }
 
-  onPressHandler(){
-    this.props.navigation.navigate('Inventory');
+  goBack = () => {this.props.navigation.navigate('Inventory')};
+
+  // TODO: style this to make it look better
+  renderImages = () => {
+    var images = [];
+    for (let i = 0; i < this.state.photos.length; i++){
+      images.push(
+        <Image key={i} style={{width: 100, height: 100}} source={{uri: this.state.photos[i]}} />
+      );
+    }
+    return images;
+  }
+
+  onAddImage = () => {
+    var options = {
+      title: 'Select Image',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+        this.setState({...this.state, warning: response.error});
+      } else {
+        this.setState({...this.state, photos: [...this.state.photos, response.uri]});
+      }
+    });
+  };
+
+  onPressHandler = () => {
+
+    const owners = this.state.owners.map(data => {return data.id;});
+    owners.push(this.props.uid);
+    const dateOwned = this.state.dateOwned.valueOf();
+
+    const itemToUpload = {
+      name: this.state.name,
+      owners: owners,
+      dateOwned: dateOwned,
+      description: this.state.description,
+      photos: this.state.photos,
+      categories: this.state.categories,
+    };
+
+    this.setState((state, props) => {
+      return {
+        ...state,
+        isUploading: true,
+        isUploadingImage: state.photos.length > 0,
+      };
+    }, () => this.addItem(itemToUpload));
+  }
+
+  addItem = data => {
+    const completeStorage = () => {this.setState({...this.state, isUploadingImage: false});};
+
+    registerItem(data, null, null, completeStorage)
+      .then(() => this.setState({
+        ...this.state,
+        isUploading: false,
+        showFinishedMessage: true,
+      }))
+      .catch(err => {
+        console.log(err);
+        this.setState({...this.state, isUploading: false, warning: err.toString()});
+      });
   }
 
   render(){
+    console.log(this.state.dateOwned.valueOf());
+
     return (
       <SafeAreaView style = {styles.viewContainer}>
+        {this.state.warning !== '' && <Text>{this.state.warning}</Text>}
+        {this.state.isUploading && <ActivityIndicator animating size="large" />}
+        {this.state.showFinishedMessage && <SafeAreaView>{this.state.finishedMessage}</SafeAreaView>}
         <ScrollView style = {styles.scrollViewContainer}>
           <Text style = {styles.textStyle}>
           Name:
@@ -39,7 +134,7 @@ export default class ItemRegistrationForm extends Component{
             onChangeText={input => this.setState({ name: input })}
             value={this.state.name}
           />
-          <Text style = {styles.textStyle}>
+          {/* <Text style = {styles.textStyle}>
           Owner:
           </Text>
           <TextInput
@@ -47,13 +142,13 @@ export default class ItemRegistrationForm extends Component{
             autoCorrect={false}
             onChangeText={input => this.setState({ owner: input })}
             value={this.state.owner}
-          />
+          /> */}
           <Text style = {styles.textStyle}>
-          Date Added:
+          Date Owned:
           </Text>
           <DatePicker
             style={styles.datePickerStyle}
-            date={this.state.date}
+            date={this.state.dateOwned}
             mode="date"
             placeholder="select date"
             format="YYYY-MM-DD"
@@ -70,9 +165,9 @@ export default class ItemRegistrationForm extends Component{
                 marginLeft: 36,
               },
             }}
-            onDateChange={(date) => {this.setState({dateAdded: date});}}
+            onDateChange={(date) => {this.setState({dateOwned: moment(date)});}}
           />
-          <Text style ={styles.textStyle}>
+          {/* <Text style ={styles.textStyle}>
           Origin:
           </Text>
           <TextInput
@@ -80,7 +175,7 @@ export default class ItemRegistrationForm extends Component{
             autoCorrect={false}
             onChangeText={input => this.setState({ origin: input })}
             value={this.state.origin}
-          />
+          /> */}
           <Text style = {styles.textStyle}>
           Description:
           </Text>
@@ -90,6 +185,9 @@ export default class ItemRegistrationForm extends Component{
             onChangeText={input => this.setState({ description: input })}
             value={this.state.description}
           />
+          <Button title="Add Pictures" onPress={this.onAddImage} />
+          {/* TODO: make the gallery look better */}
+          {this.state.photos.length > 0 && this.renderImages()}
           <TouchableOpacity
             style={styles.buttonStyle}
             onPress={this.onPressHandler}>
@@ -97,7 +195,7 @@ export default class ItemRegistrationForm extends Component{
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.buttonStyle}
-            onPress={this.onPressHandler}>
+            onPress={this.goBack}>
             <Text> Cancel </Text>
           </TouchableOpacity>
         </ScrollView>
@@ -124,5 +222,10 @@ const styles = StyleSheet.create({
   },
   buttonStyle:{
 
-  }
+  },
 });
+
+export default connect((state) => {
+  const {user} = state;
+  return {uid: user.uid, email: user.email};
+}, null)(ItemRegistrationForm);
