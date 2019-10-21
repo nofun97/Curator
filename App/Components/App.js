4985714a0a6812a7d16cdd550e378a992fb754b6
Novan <novan.allanadi1211@gmail.com>
2019-10-21 21:25:50 +1100

    stack navigator import changed

--- App/Components/App.js
+++ App/Components/App.js
@@ -1,4 +1,5 @@
-import {createAppContainer, createStackNavigator} from 'react-navigation';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
 import AccountDetailsPage from "../Pages/AccountDetailsPage";
